5b8e99765c4797d324304c35135aed3b9cda33f0
Martyn Gigg <martyn.gigg@gmail.com>
2021-12-15 16:15:11 +0000

    Consolidate setup.py to specify package name as variable

--- setup.py
+++ setup.py
@@ -1,72 +1,66 @@
+"""
+Setuptools support for building the Fortran extensions with
+numpy.f2py
+"""
+from pathlib import PurePosixPath
+# Importing setuptools modifies the behaviour of setup from distutils
+# to support building wheels. It will be marked as unused by IDEs/static analysis.
 import setuptools
-import numpy.distutils.core
 import sys
+from typing import Sequence, Tuple
 
-if sys.platform == 'win32':
-    # Compile static libs on windows
-    extra_link_args = ["-static", "-static-libgfortran", "-static-libgcc"]
-elif sys.platform == 'darwin':
-    extra_compiler_args = ['-Wno-argument-mismatch']
-    extra_link_args = ["-static", "-static-libgfortran", "-static-libgcc"]
-else:
-    # In the docker container there are no issues building the libraries, so we can assume we don't need any flags
-    extra_compiler_args = []
-    extra_link_args = []   
-
-ResNorm = numpy.distutils.core.Extension(name="quasielasticbayes.ResNorm",
-                                         sources=['quasielasticbayes/ResNorm_main.f90',
-                                                  'quasielasticbayes/ResNorm_subs.f90',
-                                                  'quasielasticbayes/BlrRes.f90',
-                                                  'quasielasticbayes/Bayes.f90',
-                                                  'quasielasticbayes/Four.f90',
-                                                  'quasielasticbayes/Util.f90'],
-                                         extra_link_args=extra_link_args,
-                                         extra_compile_args=extra_compiler_args)
-
-Quest = numpy.distutils.core.Extension(name="quasielasticbayes.Quest",
-                                       sources=['quasielasticbayes/Quest_main.f90',
-                                                'quasielasticbayes/Quest_subs.f90',
-                                                'quasielasticbayes/BlrRes.f90',
-                                                'quasielasticbayes/Bayes.f90',
-                                                'quasielasticbayes/Four.f90',
-                                                'quasielasticbayes/Util.f90',
-                                                'quasielasticbayes/Simopt.f90'],
-                                       extra_link_args=extra_link_args,
-                                       extra_compile_args=extra_compiler_args)
-
-QLse = numpy.distutils.core.Extension(name="quasielasticbayes.QLse",
-                                      sources=['quasielasticbayes/QLse_main.f90',
-                                               'quasielasticbayes/QLse_subs.f90',
-                                               'quasielasticbayes/BlrRes.f90',
-                                               'quasielasticbayes/Bayes.f90',
-                                               'quasielasticbayes/Four.f90',
-                                               'quasielasticbayes/Util.f90',
-                                               'quasielasticbayes/Simopt.f90'],
-                                        extra_link_args=extra_link_args,
-                                        extra_compile_args=extra_compiler_args)
-
-QLres = numpy.distutils.core.Extension(name="quasielasticbayes.QLres",
-                                       sources=['quasielasticbayes/QLres_main.f90',
-                                                'quasielasticbayes/QLres_subs.f90',
-                                                'quasielasticbayes/BlrRes.f90',
-                                                'quasielasticbayes/Bayes.f90',
-                                                'quasielasticbayes/Four.f90',
-                                                'quasielasticbayes/Util.f90'],
-                                        extra_link_args=extra_link_args,
-                                        extra_compile_args=extra_compiler_args)
-
-QLdata = numpy.distutils.core.Extension(name="quasielasticbayes.QLdata",
-                                        sources=['quasielasticbayes/QLdata_main.f90',
-                                                 'quasielasticbayes/QLdata_subs.f90',
-                                                 'quasielasticbayes/Bayes.f90',
-                                                 'quasielasticbayes/Four.f90',
-                                                 'quasielasticbayes/Util.f90'],
-                                        extra_link_args=extra_link_args,
-                                        extra_compile_args=extra_compiler_args)
-
-
+from numpy.distutils.core import (Extension as FortranExtension, setup)
 from numpy.distutils.command.build_ext import build_ext as _build_ext
-class build_ext(_build_ext):
+
+PACKAGE_NAME = 'quasielasticbayes'
+
+
+def create_fortran_extension(fq_name: str, sources: Sequence[str]) -> FortranExtension:
+    """
+    Create an extension module to be built by f2py
+    :param fq_name: The final fully-qualified name of the module
+    :param sources: List of relative paths from this file to the sources
+    :return: An Extension class to be built
+    """
+    extra_compile_args, extra_link_args = compiler_flags()
+    return FortranExtension(name=fq_name,
+                            sources=sources,
+                            extra_link_args=extra_link_args,
+                            extra_compile_args=extra_compile_args)
+
+
+def compiler_flags() -> Tuple[Sequence[str], Sequence[str]]:
+    """
+    :return: The compiler flags appropriate for this platform
+    """
+    if sys.platform == 'win32':
+        # Compile static libs on windows
+        extra_compile_args = []
+        extra_link_args = ["-static", "-static-libgfortran", "-static-libgcc"]
+    elif sys.platform == 'darwin':
+        extra_compile_args = ['-Wno-argument-mismatch']
+        extra_link_args = ["-static", "-static-libgfortran", "-static-libgcc"]
+    else:
+        # On Linux we build a manylinux2010
+        # (https://www.python.org/dev/peps/pep-0571/#the-manylinux2010-policy)
+        # wheel that assumes compatible versions of bases libraries are installed.
+        extra_compile_args = []
+        extra_link_args = []
+
+    return extra_compile_args, extra_link_args
+
+
+def source_paths(dirname: PurePosixPath, filenames: Sequence[str]) -> Sequence[str]:
+    """
+    :param dirname: A relative path to the list of source files
+    :return: A list of relative paths to the given sources in the directory
+    """
+    return [str(dirname / filename) for filename in filenames]
+
+
+class FortranExtensionBuilder(_build_ext):
+    """Custom extension builder to use specific compilers"""
+
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # If we don't do this on windows, when we do bdist_wheel we wont get a static link
@@ -75,18 +69,60 @@ class build_ext(_build_ext):
             self.fcompiler = 'gnu95'
             self.compiler = 'mingw32'
 
-numpy.distutils.core.setup(
-    name='quasielasticbayes',
-    install_requires=['numpy>=1.17.5'],
-    packages=['quasielasticbayes'],
+
+# Start setup
+# Create extension builders
+module_source_map = {
+    f'{PACKAGE_NAME}.ResNorm': ['ResNorm_main.f90',
+                                'ResNorm_subs.f90',
+                                'BlrRes.f90',
+                                'Bayes.f90',
+                                'Four.f90',
+                                'Util.f90'],
+    f'{PACKAGE_NAME}.Quest': ['Quest_main.f90',
+                              'Quest_subs.f90',
+                              'BlrRes.f90',
+                              'Bayes.f90',
+                              'Four.f90',
+                              'Util.f90',
+                              'Simopt.f90'],
+    f'{PACKAGE_NAME}.QLse':
+        ['QLse_main.f90',
+         'QLse_subs.f90',
+         'BlrRes.f90',
+         'Bayes.f90',
+         'Four.f90',
+         'Util.f90',
+         'Simopt.f90'],
+    f'{PACKAGE_NAME}.QLres':
+        ['QLres_main.f90',
+         'QLres_subs.f90',
+         'BlrRes.f90',
+         'Bayes.f90',
+         'Four.f90',
+         'Util.f90'],
+    f'{PACKAGE_NAME}.QLdata': ['QLdata_main.f90',
+                               'QLdata_subs.f90',
+                               'Bayes.f90',
+                               'Four.f90',
+                               'Util.f90']
+}
+extensions = [create_fortran_extension(name, source_paths(PurePosixPath('quasielasticbayes'), sources)) for
+              name, sources in module_source_map.items()]
+
+setup(
+    name=PACKAGE_NAME,
+    install_requires=['numpy>=1.12'],
+    packages=[PACKAGE_NAME],
     description='A Bayesian fitting package used for fitting quasi-elastic neutron scattering data.',
-    long_description='This package wraps fortran Bayesian fitting libraries using f2py. '\
-                     'An application of this package is to fit quasi elastic neutron scattering data in Mantid (https://www.mantidproject.org)',
+    long_description='This package wraps fortran Bayesian fitting libraries using f2py. '
+                     'An application of this package is to fit quasi-elastic'
+                     'neutron scattering data in Mantid (https://www.mantidproject.org)',
     author='Mantid Team',
-    ext_modules=[ResNorm, Quest, QLse, QLres, QLdata],
+    ext_modules=extensions,
     author_email="mantid-help@mantidproject.org",
     url='https://www.mantidproject.org',
     version="0.1.0",
     license='BSD',
-    cmdclass={'build_ext': build_ext}
+    cmdclass={'build_ext': FortranExtensionBuilder}
 )
