750909a0f01b73b6619b049efc772603de6c5eb6
Anthony Lim <anthony.lim@stfc.ac.uk>
2022-02-25 13:34:53 +0000

    refs #5 added FFT tests based on what is used

--- quasielasticbayes/Four_main.f90
+++ quasielasticbayes/Four_main.f90
@@ -4,11 +4,17 @@
       INCLUDE 'options.f90'
       
 	  INTEGER N(1), ISIGN, NDIM, IFORM
-	  COMPLEX DATA(4), y_out(4)
+	  COMPLEX DATA(4098), y_out(4098)
       real x_in(m_d), y_in(m_d), e_in(m_d)
 cf2py intent(in) :: DATA, N, ISIGN, NDIM, IFORM
 cf2py intent(out) :: y_out                               !real parameters
       y_out = DATA
+      OPEN(UNIT=11,FILE='C:\Users\BTR75544\work\f70.txt')
+      WRITE(11,*) 'hi ', ISIGN	  
+      DO KK = 1,8
+      WRITE(11,*) y_out(KK), "   ", DATA(kk),"   ", KK
+      END DO
+      CLOSE(11)	  
 	  CALL FOUR2(y_out,N,NDIM,ISIGN,IFORM)
       RETURN
       END
