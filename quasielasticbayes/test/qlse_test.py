1e8e491bbf01ee038690c45c5de79087df3afe24
Anthony <anthony.lim@stfc.ac.uk>
2022-03-11 16:05:22 +0000

    updated new tests to auto clean up

--- quasielasticbayes/test/qlse_test.py
+++ quasielasticbayes/test/qlse_test.py
@@ -8,15 +8,7 @@ from quasielasticbayes.QLres import qlres
 import json
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
+
 class QLresTest(unittest.TestCase):
     """
     Characterization tests using inputs that have been accepted as correct.
@@ -28,24 +20,24 @@ class QLresTest(unittest.TestCase):
         # reference inputs
         with open(os.path.join(DATA_DIR, 'qlse', 'qlse_input.json'), 'r') as fh:
             inputs = load_json(fh)
-        temp_dir = tempfile.TemporaryDirectory()	
-        inputs['wrks'] = add_path(temp_dir.name, inputs['wrks'])
-        nd, xout, yout, eout, yfit, yprob = qlres(inputs['numb'], inputs['Xv'], inputs['Yv'], inputs['Ev'],
-                                                  inputs['reals'], inputs['fitOp'],
-                                                  inputs['Xdat'], inputs['Xb'], inputs['Yb'],
-                                                  inputs['Wy'], inputs['We'], inputs['dtn'], inputs['xsc'],
-                                                  inputs['wrks'], inputs['wrkr'], inputs['lwrk'])
+        with tempfile.TemporaryDirectory() as tmp_dir:	
 
-        # verify
-        with open(os.path.join(DATA_DIR, 'qlse', 'qlse_output.json'), 'r') as fh:
-            reference = load_json(fh)
+            inputs['wrks'] = add_path(tmp_dir, inputs['wrks'])
+            nd, xout, yout, eout, yfit, yprob = qlres(inputs['numb'], inputs['Xv'], inputs['Yv'], inputs['Ev'],
+                                                      inputs['reals'], inputs['fitOp'],
+                                                      inputs['Xdat'], inputs['Xb'], inputs['Yb'],
+                                                      inputs['Wy'], inputs['We'], inputs['dtn'], inputs['xsc'],
+                                                      inputs['wrks'], inputs['wrkr'], inputs['lwrk'])
+
+            # verify
+            with open(os.path.join(DATA_DIR, 'qlse', 'qlse_output.json'), 'r') as fh:
+                reference = load_json(fh)
         
-        self.assertEqual(reference['nd'], nd)
-        np.testing.assert_almost_equal(reference['xout'], xout)
-        np.testing.assert_almost_equal(reference['yout'], yout)
-        np.testing.assert_almost_equal(reference['eout'], eout)
-        np.testing.assert_almost_equal(reference['yfit'], yfit)
-        np.testing.assert_almost_equal(reference['yprob'], yprob)
-        temp_dir.cleanup()
+            self.assertEqual(reference['nd'], nd)
+            np.testing.assert_almost_equal(reference['xout'], xout)
+            np.testing.assert_almost_equal(reference['yout'], yout)
+            np.testing.assert_almost_equal(reference['eout'], eout)
+            np.testing.assert_almost_equal(reference['yfit'], yfit)
+            np.testing.assert_almost_equal(reference['yprob'], yprob)
 if __name__ == '__main__':
     unittest.main()
