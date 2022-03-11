95088a376c67ba5081f83c1730b738842b21bdfd
Anthony <anthony.lim@stfc.ac.uk>
2022-03-11 12:38:00 +0000

    refs #7 update test to auto close tmp dir

--- quasielasticbayes/test/qlres_test.py
+++ quasielasticbayes/test/qlres_test.py
@@ -20,24 +20,24 @@ class QLresTest(unittest.TestCase):
         # reference inputs
         with open(os.path.join(DATA_DIR, 'qlres', 'qlres-input-spec-0.json'), 'r') as fh:
             inputs = load_json(fh)
-        temp_dir = tempfile.TemporaryDirectory()	
-        inputs['wrks'] = add_path(temp_dir.name, inputs['wrks'])
-        nd, xout, yout, eout, yfit, yprob = qlres(inputs['numb'], inputs['Xv'], inputs['Yv'], inputs['Ev'],
-                                                  inputs['reals'], inputs['fitOp'],
-                                                  inputs['Xdat'], inputs['Xb'], inputs['Yb'],
-                                                  inputs['Wy'], inputs['We'], inputs['dtn'], inputs['xsc'],
-                                                  inputs['wrks'], inputs['wrkr'], inputs['lwrk'])
+        with tempfile.TemporaryDirectory() as tmp_dir:	
+            inputs['wrks'] = add_path(tmp_dir, inputs['wrks'])
+            nd, xout, yout, eout, yfit, yprob = qlres(inputs['numb'], inputs['Xv'], inputs['Yv'], inputs['Ev'],
+                                                      inputs['reals'], inputs['fitOp'],
+                                                      inputs['Xdat'], inputs['Xb'], inputs['Yb'],
+                                                      inputs['Wy'], inputs['We'], inputs['dtn'], inputs['xsc'],
+                                                      inputs['wrks'], inputs['wrkr'], inputs['lwrk'])
 
-        # verify
-        with open(os.path.join(DATA_DIR, 'qlres', 'qlres-output-spec-0.json'), 'r') as fh:
-            reference = load_json(fh)
+            # verify
+            with open(os.path.join(DATA_DIR, 'qlres', 'qlres-output-spec-0.json'), 'r') as fh:
+                reference = load_json(fh)
         
-        self.assertEqual(reference['nd'], nd)
-        np.testing.assert_almost_equal(reference['xout'], xout)
-        np.testing.assert_almost_equal(reference['yout'], yout)
-        np.testing.assert_almost_equal(reference['eout'], eout)
-        np.testing.assert_almost_equal(reference['yfit'], yfit)
-        np.testing.assert_almost_equal(reference['yprob'], yprob)
-        temp_dir.cleanup()
+            self.assertEqual(reference['nd'], nd)
+            np.testing.assert_almost_equal(reference['xout'], xout)
+            np.testing.assert_almost_equal(reference['yout'], yout)
+            np.testing.assert_almost_equal(reference['eout'], eout)
+            np.testing.assert_almost_equal(reference['yfit'], yfit)
+            np.testing.assert_almost_equal(reference['yprob'], yprob)
+        #tmp_dir.cleanup() done on exiting the with statment
 if __name__ == '__main__':
     unittest.main()
