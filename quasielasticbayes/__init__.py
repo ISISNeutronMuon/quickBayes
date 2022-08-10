5696859966bed7e4b7c22e61d9646f03cbe3c8ca
Anthony <anthony.lim@stfc.ac.uk>
2022-08-10 17:38:23 +0100

    refs #11 need this to make it work

--- quasielasticbayes/__init__.py
+++ quasielasticbayes/__init__.py
@@ -1,4 +1,4 @@
 # SPDX - License - Identifier: BSD-3-Clause
 
 # --- Public API --------------------------------------------------------------
-from quasielasticbayes import QLdata, QLres, ResNorm, QLse # noqa F401
+from . import QLdata, QLres, ResNorm, QLse # noqa F401
