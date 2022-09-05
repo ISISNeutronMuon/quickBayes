cad1751541c1e41f1749d9fe3818c110966d9926
Anthony Lim <anthony.lim@stfc.ac.uk>
2022-09-05 16:08:51 +0100

    cython compile test

---
+++ cython_setup.py
@@ -0,0 +1,49 @@
+# from numpy.distutils.command.build_ext import build_ext as _build_ext
+# from numpy.distutils.command.build_src import build_src as _build_src
+# from distutils.command.build import build
+from distutils.core import Extension
+from Cython.Compiler import Options
+from os.path import join
+# Importing setuptools modifies the behaviour of setup from distutils
+# to support building wheels. It will be marked as unused by IDEs/static
+# analysis.
+# import setuptools
+# import sys
+from typing import Sequence
+
+# import copy
+import numpy
+
+
+def create_extension(fq_name: str,
+                             sources: Sequence[str]) -> Extension:
+    """
+    Create an extension module to be built by f2py
+    :param fq_name: The final fully-qualified name of the module
+    :param sources: List of relative paths from this file to the sources
+    :return: An Extension class to be built
+    """
+    lang = 'C'
+    Options.annotate = True
+    return Extension(name=fq_name,
+                     sources=sources,
+                     include_dirs=[numpy.get_include()],
+                     language=lang)
+
+
+def source_paths(dirname: str, filenames: Sequence[str]) -> Sequence[str]:
+    """
+    :param dirname: A relative path to the list of source files
+    :return: A list of relative paths to the given sources in the directory
+    """
+    return [join(dirname, filename) for filename in filenames]
+
+
+def get_cython_extensions(PACKAGE_NAME):
+
+    module_source_map = {
+        f'{PACKAGE_NAME}.stuff':
+            ['stuff.pyx']}
+    return [create_extension(name,
+            source_paths(str(join('src', 'c_python')), sources)) for
+            name, sources in module_source_map.items()]
--- setup.py
+++ setup.py
@@ -2,137 +2,63 @@
 Setuptools support for building the Fortran extensions with
 numpy.f2py
 """
-from os.path import join
+# from os.path import join
 # Importing setuptools modifies the behaviour of setup from distutils
 # to support building wheels. It will be marked as unused by IDEs/static
 # analysis.
 # import setuptools
-import sys
-from typing import Sequence, Tuple
+# import sys
+# from typing import Sequence, Tuple
 
-from numpy.distutils.core import (Extension as FortranExtension, setup)
-from numpy.distutils.command.build_ext import build_ext as _build_ext
+from numpy.distutils.core import setup
+
+from fortran_setup import get_fortran_extensions, FortranExtensionBuilder
+from cython_setup import get_cython_extensions
+from numpy.distutils.command.build_src import build_src as _build_src
+
 
 PACKAGE_NAME = 'quasielasticbayes'
 
-
-def create_fortran_extension(fq_name: str,
-                             sources: Sequence[str]) -> FortranExtension:
-    """
-    Create an extension module to be built by f2py
-    :param fq_name: The final fully-qualified name of the module
-    :param sources: List of relative paths from this file to the sources
-    :return: An Extension class to be built
-    """
-    (extra_compile_args, extra_f90_compile_args,
-     extra_link_args) = compiler_flags()
-    return FortranExtension(name=fq_name,
-                            sources=sources,
-                            extra_f90_compile_args=extra_f90_compile_args,
-                            extra_link_args=extra_link_args,
-                            extra_compile_args=extra_compile_args)
+extensions = (get_fortran_extensions(PACKAGE_NAME)
+              + get_cython_extensions(PACKAGE_NAME))
 
 
-def compiler_flags() -> Tuple[Sequence[str], Sequence[str], Sequence[str]]:
-    """
-    :return: The compiler flags appropriate for this platform
-    """
-    if sys.platform == 'win32':
-        # Compile static libs on windows
-        extra_compile_args = []
-        extra_f90_compile_args = ["-O0"]
-        extra_link_args = ["-static-libgfortran", "-static-libgcc"]
-    elif sys.platform == 'darwin':
-        extra_compile_args = ['-Wno-argument-mismatch']
-        extra_f90_compile_args = ["-ff2c"]
-        extra_link_args = ['-dynamiclib', '-lgfortran',
-                           '-Bstatic', '-static-libgfortran',
-                           "-static-libgcc"]
-    else:
-        # On Linux we build a manylinux2010
-        # (https://www.python.org/dev/peps/pep-0571/#the-manylinux2010-policy)
-        # wheel that assumes compatible versions of bases libraries are
-        # installed.
-        extra_compile_args = []
-        extra_f90_compile_args = ["-fallow-argument-mismatch"]
-        extra_link_args = []
+class build_source(_build_src):
 
-    return extra_compile_args, extra_f90_compile_args, extra_link_args
+    def filter_files(self, sources, exts=[]):
+        # cython causes an empty 'list' to be passed
+        # so we check that the 'list' is not empty
+        if sources == [[]]:
+            return [], []
+        return super().filter_files(sources, exts)
 
 
-def source_paths(dirname: str, filenames: Sequence[str]) -> Sequence[str]:
-    """
-    :param dirname: A relative path to the list of source files
-    :return: A list of relative paths to the given sources in the directory
-    """
-    return [join(dirname, filename) for filename in filenames]
+# from Cython.Distutils import build_ext as cython_build_ext
+# import copy
 
+class extension_builder(FortranExtensionBuilder):
+    def initialize_options(self):
+        super().initialize_options()
+        # self.cython_dist = copy.deepcopy(self.distribution)
+        # self.cython_build = cython_build_ext(self.cython_dist)
+        # self.cython_build.initialize_options()
 
-class FortranExtensionBuilder(_build_ext):
-    """Custom extension builder to use specific compilers"""
+    def finialize_options(self):
+        super().finalize_options()
+        # self.cython_build.finialize_options()
 
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # If we don't do this on windows, when we do
-        # bdist_wheel we wont get a static link
-        # this is because it misses the compiler flags to f2py
-        # which means it ignores the static flags we try to pass
-        if sys.platform == 'win32':
-            self.fcompiler = 'gnu95'
-            self.compiler = 'mingw32'
+    def run(self):
+        super().run()
 
+        # tmp = self.extensions
+        # c_ext = []
+        # for ext in tmp:
+        #    name = ext.name
+        #    if "stuff" in name:
+        #        c_ext.append(ext)
 
-# Start setup
-# Create extension builders
-module_source_map = {
-    f'{PACKAGE_NAME}.ResNorm':
-        ['ResNorm_main.f90',
-         'ResNorm_subs.f90',
-         'BlrRes.f90',
-         'Bayes.f90',
-         'Four.f90',
-         'Util.f90'],
-
-    f'{PACKAGE_NAME}.Quest':
-        ['Quest_main.f90',
-         'Quest_subs.f90',
-         'BlrRes.f90',
-         'Bayes.f90',
-         'Four.f90',
-         'Util.f90',
-         'Simopt.f90'],
-
-    f'{PACKAGE_NAME}.QLse':
-        ['QLse_main.f90',
-         'QLse_subs.f90',
-         'BlrRes.f90',
-         'Bayes.f90',
-         'Four.f90',
-         'Util.f90',
-         'Simopt.f90'],
-
-    f'{PACKAGE_NAME}.QLres':
-        ['QLres_main.f90',
-         'QLres_subs.f90',
-         'BlrRes.f90',
-         'Bayes.f90',
-         'Four.f90',
-         'Util.f90'],
-
-    f'{PACKAGE_NAME}.QLdata':
-        ['QLdata_main.f90',
-         'QLdata_subs.f90',
-         'Bayes.f90',
-         'Four.f90',
-         'Util.f90'],
-
-    f'{PACKAGE_NAME}.Four':
-        ['Four_main.f90',
-         'Four.f90']
-}
-extensions = [create_fortran_extension(name,
-              source_paths(str(join('src', PACKAGE_NAME)), sources)) for
-              name, sources in module_source_map.items()]
+        # self.cython_build.extensions = c_ext
+        # self.cython_build.run()
 
 
 setup(
@@ -154,5 +80,6 @@ setup(
     version="0.1.1",
     license='BSD',
     package_dir={'': 'src'},  # allows setup to find py and f90 files
-    cmdclass={'build_ext': FortranExtensionBuilder}
+    cmdclass={'build_ext': extension_builder, 'build_src': build_source}
+    # cmdclass={'build_ext': FortranExtensionBuilder}
 )
