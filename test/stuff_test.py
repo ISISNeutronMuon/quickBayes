848b905b27e8125f41b43640516553eea963accc
Anthony Lim <anthony.lim@stfc.ac.uk>
2022-09-07 10:36:30 +0100

    data.py

--- test/stuff_test.py
+++ test/stuff_test.py
@@ -1,11 +1,13 @@
 """Characterization tests for QLres module"""
 import unittest
-from quasielasticbayes.stuff import primes
+from quasielasticbayes.data import DatCom
 
 
 class StuffTest(unittest.TestCase):
     def test_primes(self):
-        self.assertEqual([2, 3, 5], primes(3))
+        a = DatCom
+        self.assert(a)
+        self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
