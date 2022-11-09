refs #36 fixed mistake in loglikelihood and added a test

src/quasielasticbayes/v2_python/log_likelihood.py
@@ -4,7 +4,7 @@ from math import exp, log10
 
 
 def loglikelihood(x_len: ndarray, chi2: float, log_hess_det: float,
-                  N_peaks: int) -> float:
+                  N_peaks: int, beta: float) -> float:
     """
     Calculate the unnormalised logliklihood.
     log_10 probabilities -> priors
@@ -13,32 +13,37 @@ def loglikelihood(x_len: ndarray, chi2: float, log_hess_det: float,
     "Data Analysis A bayesian tutorial second edition",
     by D. S. Sivia
     equation 4.20 page 88:
-    P(M|{D_k},I) prop \\frac{M! (4\\pi)^M}{\\beta^M\\sqrt(\\det(H))}\\exp{(-\\frac{\\chi^2}{2})}  # noqa E501
-    where \\beta = (x_{max}-x_{min})A_{max}
+    P(M|{D_k},I) prop \\frac{N! (4\\pi)^N}{\\beta^N\\sqrt(\\det(H))}\\exp{(-\\frac{\\chi^2}{2})}  # noqa E501
+    where \\beta = (x_{max}-x_{min})A_{max}, H is the Hessian matrix
 
     We want this as a logliklihood, so take \\log_{10} and use:
     - \\log_{10}(\\exp{\\alpha}) = \\ln(\\exp{\\alpha})\\log_{10}(\\exp{1})
     \\log_{10}(\\exp({\\alpha}) = \\alpha\\log_{10}(\\exp{1})
-    - N! = \\Pi_{i=0}{N} i => \\log{N!} = \\sum_{i=0}^N i
+    - N! = \\Pi_{i=0}{N} i => \\log{N!} = \\sum_{i=0}^N \\log(i)
     - \\log(ab) = \\log(a) + \\log(b)
     - \\log(\\frac{a}{b}) = \\log(a) - \\log(b)
     - \\log(a^N) = N\\log(a)
+
+    to get:
+    \\sum_{j=1}{N}\\log(j) + N\\log(4\\pi) - 0.5\\chi^2\\log(exp(1))
+    - N\\log(\\beta) - 0.5\\log(\\det(H))
 
     :param x_len: the length of the x data
     :param chi2: the chi squared for the fit
     :param log_hess_det: the log_10 of the determinant of the hessian matrix
     :param N_peaks: the number of peaks
+    :param beta: the scale factor, A_max*(x_max - x_min) eq. 4.17
     :return the loglikelihood
     """
 
     # want the unscaled chi^2 -> multiple by length of data
-    log_chi2 = -log10(exp(1.))*chi2*float(x_len)/2.
+    log_chi2 = log10(exp(1.))*chi2*float(x_len)/2.
 
-    log_likelihood = log_chi2 - .5*(log_hess_det - float(N_peaks*2))
-    log_likelihood -= float(N_peaks)
-    # cost of more peaks
-    log_likelihood += np.sum(
+    log_likelihood = np.sum(
         np.log10(np.asarray([k + 1 for k in range(N_peaks)])))
     log_likelihood += float(N_peaks)*log10(4.*np.pi)
+    log_likelihood -= log_chi2
+    log_likelihood -= float(N_peaks)*log10(beta)
+    log_likelihood -= 0.5*(log_hess_det)
 
     return log_likelihood

src/quasielasticbayes/v2_python/qldata_main.py
@@ -12,7 +12,9 @@ from typing import Dict, List
 
 def ql_data_main(sample: Dict[str, ndarray], res: Dict[str, ndarray],
                  BG_type: str, start_x: float, end_x: float,
-                 elastic: bool) -> (Dict[str, ndarray], List[float]):
+                 elastic: bool,
+                 results: Dict[str, ndarray]) -> (Dict[str, ndarray],
+                                                  List[float]):
     """
     The main function for calculating Qldata.
     Steps are:
@@ -30,10 +32,10 @@ def ql_data_main(sample: Dict[str, ndarray], res: Dict[str, ndarray],
     :param start_x: the start x for the calculation
     :param end_x: the end x for the calculation
     :param elastic: if to include the elastic peak
+    :param results: dict of results
     :result dict of the fit parameters and an array of the loglikelihoods
     """
     # step 0
-    results = {}
     BG = get_background_function(BG_type)
     max_num_peaks = 3
 
@@ -46,6 +48,7 @@ def ql_data_main(sample: Dict[str, ndarray], res: Dict[str, ndarray],
     se = spline(sample['x'], sample['e'], new_x)
     ry = spline(res['x'], res['y'], new_x)
 
+    beta = np.max(sy)*(np.max(new_x)-np.min(new_x))
     func = QlDataFunction(BG, elastic, new_x, ry, start_x, end_x)
     guess = func.get_guess()
     # loop doing steps 2 to 8
@@ -62,14 +65,14 @@ def ql_data_main(sample: Dict[str, ndarray], res: Dict[str, ndarray],
 
         results = func.report(results, *params)
 
-        prob_name = 'N{N}:loglikelihood'
+        prob_name = f'N{N}:loglikelihood'
         if prob_name in results:
             results[prob_name].append(loglikelihood(len(sy), chi2,
                                                     hess_det,
-                                                    func.N_peaks))
+                                                    func.N_peaks, beta))
         else:
-            results[f'N{N}:loglikelihood'] = [loglikelihood(len(sy), chi2,
-                                                            hess_det,
-                                                            func.N_peaks)]
+            results[prob_name] = [loglikelihood(len(sy), chi2,
+                                                hess_det,
+                                                func.N_peaks, beta)]
 
     return results, new_x

test/qldatav2_test.py
@@ -18,14 +18,15 @@ class QlDataV2Test(unittest.TestCase):
 
         sample = {'x': sx, 'y': sy, 'e': se}
         resolution = {'x': rx, 'y': ry}
+        results = {}
 
         results, new_x = ql_data_main(sample, resolution,
-                                      "linear", -0.4, 0.4, True)
+                                      "linear", -0.4, 0.4, True, results)
 
         # not from Mantid
-        self.assertAlmostEqual(results['N1:loglikelihood'][0], -659.23, 2)
-        self.assertAlmostEqual(results['N2:loglikelihood'][0], -348.82, 2)
-        self.assertAlmostEqual(results['N3:loglikelihood'][0], -350.36, 2)
+        self.assertAlmostEqual(results['N1:loglikelihood'][0], -659.4, 2)
+        self.assertAlmostEqual(results['N2:loglikelihood'][0], -349.17, 2)
+        self.assertAlmostEqual(results['N3:loglikelihood'][0], -350.87, 2)
 
         # from Mantid, if not then as a comment
         self.assertAlmostEqual(results['N1:f2.f2.Gamma'][0], 0.0566, 2)
@@ -50,6 +51,32 @@ class QlDataV2Test(unittest.TestCase):
         self.assertAlmostEqual(results['N3:f2.f2.EISF'][0], 0.103, 2)  # 0.0401
         self.assertAlmostEqual(results['N3:f2.f3.EISF'][0], 0.021, 2)  # 0.0085
         self.assertAlmostEqual(results['N3:f2.f4.EISF'][0], 0.183, 2)  # 0.0668
+
+    def test_two(self):
+        """
+        Want to check that two calls to the function will append the results
+        correctly. So if we use the same input data as above, we expect
+        both values to be the same for every item in the dict.
+        """
+        sx, sy, se = np.load(os.path.join(DATA_DIR, 'sample_data_red.npy'))
+        rx, ry, re = np.load(os.path.join(DATA_DIR, 'resolution_data_red.npy'))
+
+        sample = {'x': sx, 'y': sy, 'e': se}
+        resolution = {'x': rx, 'y': ry}
+        results = {}
+
+        results, new_x = ql_data_main(sample, resolution,
+                                      "linear", -0.4, 0.4, True, results)
+
+        # call it again
+        results, new_x = ql_data_main(sample, resolution,
+                                      "linear", -0.4, 0.4, True, results)
+
+        for key in results.keys():
+            print(key, results[key])
+            self.assertEqual(len(results[key]), 2)
+            tmp = results[key]
+            self.assertEqual(tmp[0], tmp[1])
 
 
 if __name__ == '__main__':