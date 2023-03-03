c2a0064a6a3953f87e90b78b5f341af57777923e
Anthony Lim <anthony.lim@stfc.ac.uk>
2023-03-03 13:55:32 +0000

    refs #47 code clean up and tests

--- test/workflows/workflowTempleta_test.py
+++ test/workflows/workflowTempleta_test.py
@@ -1,8 +1,19 @@
 import unittest
 from quasielasticbayes.v2.workflow.template import Workflow
-from quasielasticbayes.v2.functions.BG import LinearBG, FlatBG
+from quasielasticbayes.v2.functions.BG import FlatBG
+from quasielasticbayes.v2.functions.exp_decay import ExpDecay
 from quasielasticbayes.v2.functions.composite import CompositeFunction
-from quasielasticbayes.test_helpers.fitting_data import basic_data
+import numpy as np
+
+
+def gen_data():
+    x = np.linspace(1, 4, 100)
+    np.random.seed(1)
+    noise_stdev = 0.1
+    y = np.random.normal(0.5 +
+                         1.2*np.exp(-2*x), noise_stdev)
+    e = 0.5*noise_stdev*np.ones(x.shape)
+    return x, y, e
 
 
 class SimpleWorkflow(Workflow):
@@ -10,8 +21,8 @@ class SimpleWorkflow(Workflow):
         """
         Add a flat BG term
         """
-        bg = FlatBG()
-        func.add_function(bg)
+        ed = ExpDecay()
+        func.add_function(ed)
         return func
 
 
@@ -19,14 +30,14 @@ class WorkflowTemplateTest(unittest.TestCase):
 
     def setUp(self):
         self.func = CompositeFunction()
-        lin = LinearBG()
+        lin = FlatBG()
         self.func.add_function(lin)
         results = {}
         errors = {}
         self.wf = SimpleWorkflow(results, errors)
 
     def test_preprocess_data(self):
-        x, y, e = basic_data()
+        x, y, e = gen_data()
         self.wf.preprocess_data(x, y, e)
         self.assertEqual(len(self.wf._data), 3)
         self.assertEqual(len(self.wf._data['x']), len(x))
@@ -42,7 +53,7 @@ class WorkflowTemplateTest(unittest.TestCase):
         Going to use get_guess to tell if the
         function has had anything added to it
         """
-        self.assertEqual(len(self.func.get_guess()), 2)
+        self.assertEqual(len(self.func.get_guess()), 1)
         self.assertEqual(self.func._prefix, '')
         func = self.wf.update_function(self.func, 1)
         self.assertEqual(len(self.func.get_guess()), 3)
@@ -55,7 +66,7 @@ class WorkflowTemplateTest(unittest.TestCase):
         self.assertEqual(errors, {})
 
         # setup workflow + generate data
-        x, y, e = basic_data()
+        x, y, e = gen_data()
         self.wf.preprocess_data(x, y, e)
         self.wf.set_scipy_engine([0], [-9], [9])
         _ = self.wf.execute(1, self.func)
@@ -63,13 +74,13 @@ class WorkflowTemplateTest(unittest.TestCase):
         params, errors = self.wf.get_parameters_and_errors
         self.assertEqual(len(params), 4)
         self.assertEqual(len(errors), 3)
-        expected_keys = ['N1:f1.BG gradient',
-                         'N1:f1.BG constant',
-                         'N1:f2.BG constant']
+        expected_keys = ['N1:f1.BG constant',
+                         'N1:f2.Amplitude',
+                         'N1:f2.lambda']
 
         self.assertEqual(list(errors.keys()), expected_keys)
-        expected_param = [0.986, 0.061, 0.061]
-        expected_error = [0.045, 0.041, 0.041]
+        expected_param = [0.514, 1.000, 2.110]
+        expected_error = [0.008, 0.913, 0.802]
         for j, key in enumerate(expected_keys):
             self.assertAlmostEqual(params[key][0], expected_param[j], 3)
             self.assertAlmostEqual(errors[key][0], expected_error[j], 3)
@@ -77,20 +88,20 @@ class WorkflowTemplateTest(unittest.TestCase):
         prob_key = 'N1:loglikelihood'
         expected_keys.append(prob_key)
         self.assertEqual(list(params.keys()), expected_keys)
-        self.assertEqual(type(params[prob_key][0]), float)
+        self.assertAlmostEqual(params[prob_key][0], -72., 0)
 
     def test_fails_if_no_data(self):
         with self.assertRaises(ValueError):
             self.wf.set_scipy_engine([0], [-9], [9])
 
     def test_execute_no_engine(self):
-        x, y, e = basic_data()
+        x, y, e = gen_data()
         self.wf.preprocess_data(x, y, e)
         with self.assertRaises(ValueError):
             _ = self.wf.execute(1, self.func)
 
     def test_add_second_engine_errors(self):
-        x, y, e = basic_data()
+        x, y, e = gen_data()
         self.wf.preprocess_data(x, y, e)
         self.wf.set_scipy_engine([0], [-9], [9])
         with self.assertRaises(RuntimeError):
@@ -98,7 +109,7 @@ class WorkflowTemplateTest(unittest.TestCase):
 
     def test_set_scipy_engine(self):
         self.assertEqual(self.wf.fit_engine, None)
-        x, y, e = basic_data()
+        x, y, e = gen_data()
         self.wf.preprocess_data(x, y, e)
         self.wf.set_scipy_engine([], [], [])
         self.assertEqual(self.wf.fit_engine.name, 'scipy')
