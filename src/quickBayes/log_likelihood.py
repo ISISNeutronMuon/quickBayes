21c152655e79ea9b8c460901fa4371f3bb87348c
Anthony Lim <anthony.lim@stfc.ac.uk>
2023-04-05 10:20:40 +0100

    refs #59 first doc changes

--- src/quickBayes/log_likelihood.py
+++ src/quickBayes/log_likelihood.py
@@ -8,26 +8,26 @@ def loglikelihood(x_len: ndarray, chi2: float, covar: ndarray,
                   N_peaks: int, beta: float) -> float:
     """
     Calculate the unnormalised logliklihood.
-    log_10 probabilities -> priors
+        log_10 probabilities -> priors
 
     The equation for the probability is taken from
-    "Data Analysis A bayesian tutorial second edition",
-    by D. S. Sivia
-    equation 4.20 page 88:
-    P(M|{D_k},I) prop \\frac{N! (4\\pi)^N}{\\beta^N\\sqrt(\\det(H))}\\exp{(-\\frac{\\chi^2}{2})}  # noqa E501
-    where \\beta = (x_{max}-x_{min})A_{max}, H is the Hessian matrix
+        "Data Analysis A bayesian tutorial second edition",
+        by D. S. Sivia
+        equation 4.20 page 88:
+        P(M|{D_k},I) prop \\frac{N! (4\\pi)^N}{\\beta^N\\sqrt(\\det(H))}\\exp{(-\\frac{\\chi^2}{2})}  # noqa E501
+        where \\beta = (x_{max}-x_{min})A_{max}, H is the Hessian matrix
 
     We want this as a logliklihood, so take \\log_{10} and use:
-    - \\log_{10}(\\exp{\\alpha}) = \\ln(\\exp{\\alpha})\\log_{10}(\\exp{1})
-    \\log_{10}(\\exp({\\alpha}) = \\alpha\\log_{10}(\\exp{1})
-    - N! = \\Pi_{i=0}{N} i => \\log{N!} = \\sum_{i=0}^N \\log(i)
-    - \\log(ab) = \\log(a) + \\log(b)
-    - \\log(\\frac{a}{b}) = \\log(a) - \\log(b)
-    - \\log(a^N) = N\\log(a)
+        - \\log_{10}(\\exp{\\alpha}) = \\ln(\\exp{\\alpha})\\log_{10}(\\exp{1})
+        \\log_{10}(\\exp({\\alpha}) = \\alpha\\log_{10}(\\exp{1})
+        - N! = \\Pi_{i=0}{N} i => \\log{N!} = \\sum_{i=0}^N \\log(i)
+        - \\log(ab) = \\log(a) + \\log(b)
+        - \\log(\\frac{a}{b}) = \\log(a) - \\log(b)
+        - \\log(a^N) = N\\log(a)
 
     to get:
-    \\sum_{j=1}{N}\\log(j) + N\\log(4\\pi) - 0.5\\chi^2\\log(exp(1))
-    - N\\log(\\beta) - 0.5\\log(\\det(H))
+        \\sum_{j=1}{N}\\log(j) + N\\log(4\\pi) - 0.5\\chi^2\\log(exp(1))
+        - N\\log(\\beta) - 0.5\\log(\\det(H))
 
     :param x_len: the length of the x data
     :param chi2: the chi squared for the fit
