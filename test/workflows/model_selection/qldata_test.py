0931cb798f5c5216df2aef0f581b8d2aeef57891
Anthony <anthony.lim@stfc.ac.uk>
2023-08-25 11:06:40 +0100

    fix EISF error report (#72)
    * fix EISF error report
    
    * fix EISF error report
    
    * fix EISF error report
    
    * fix EISF error report
    
    * fix EISF error report
    
    * fix EISF error report
    
    * fix EISF error report

--- test/workflows/model_selection/qldata_test.py
+++ test/workflows/model_selection/qldata_test.py
@@ -98,11 +98,11 @@ class QlDataTest(unittest.TestCase):
         self.assertAlmostEqual(errors['N1:f2.f2.EISF'][0], 0.006, 3)
 
         self.assertAlmostEqual(errors['N2:f2.f2.EISF'][0], 0.02, 2)
-        self.assertAlmostEqual(errors['N2:f2.f3.EISF'][0], 0.002, 3)
+        self.assertAlmostEqual(errors['N2:f2.f3.EISF'][0], 0.011, 3)
 
         self.assertAlmostEqual(errors['N3:f2.f2.EISF'][0], 0.4, 1)
-        self.assertAlmostEqual(errors['N3:f2.f3.EISF'][0], 0.02, 2)
-        self.assertAlmostEqual(errors['N3:f2.f4.EISF'][0], 0.3, 1)
+        self.assertAlmostEqual(errors['N3:f2.f3.EISF'][0], 0.116, 2)
+        self.assertAlmostEqual(errors['N3:f2.f4.EISF'][0], 0.03, 2)
 
     def test_two(self):
         """
