503e8436807aa112fdaa4a3bc47efbe9eaedf0b9
Robert <robert.applin@stfc.ac.uk>
2023-10-13 09:39:44 +0100

    Add script to remove previous build files

---
+++ tools/clean.sh
@@ -0,0 +1,9 @@
+# Clean previous build files
+rm -rf build
+rm -rf src/quickBayes.egg-info
+rm -rf dist
+rm src/quickBayes/*.c
+rm src/quickBayes/*/*.c
+rm src/quickBayes/*/*/*.c
+rm src/quickBayes/*.so
+rm src/quickBayes/*/*.so
