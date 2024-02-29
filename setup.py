e6bff1f17e2014fabb70131a872271d62fa6b6c9
Anthony Lim <anthony.lim@stfc.ac.uk>
2024-02-29 13:49:22 +0000

    #refs 89 first go at fixing pip install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from tools.setup_helper import get_extensions
 
 
-VERSION = "1.0.0b20"
+VERSION = "1.0.0b21"
 
 
 PACKAGE_NAME = 'quickBayes'
@@ -34,5 +34,6 @@ setup(
     url='https://www.mantidproject.org',
     version=VERSION,
     license='BSD',
-    package_dir={'': 'src', 'tools': 'tools'}
+    data_files=[('.', ['tools/*'])],
+    package_dir={'': 'src'}
 )
